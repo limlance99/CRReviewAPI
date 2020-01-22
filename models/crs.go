f892b94ba51e3022a22fdd30c841c9ffc22f092f
limlance99 <limlance_99@yahoo.com>
2020-01-22 17:56:19 +0800

    added postCRs

--- models/crs.go
+++ models/crs.go
@@ -8,7 +8,7 @@ type CR struct {
 	CreatedAt  time.Time `json:"createdat"`
 	Location   Location  `gorm:"foreignkey:LocationID;association_foreignkey:LocationID" json:"location"`
 	Reviews    []Review  `gorm:"foreignkey:CRid;association_foreignkey:CRid" json:"reviews"`
-	LocationID int       `gorm:"column:locationid; not null" json:"locationid"`
+	LocationID uint      `gorm:"column:locationid; not null" json:"locationid"`
 	Floor      int       `gorm:"column:floor; type:int; not null" json:"floor"`
 	Gender     string    `gorm:"column:gender; type:char(1); not null" json:"gender"`
 }
--- queries/post.go
+++ queries/post.go
@@ -9,10 +9,30 @@ import (
 // PostCRs adds a new CR record to the database
 // <url>/api/crs
 func PostCRs(c echo.Context) error {
+	type data struct {
+		locationid uint
+		floor      int
+		gender     string
+		facilities []uint
+	}
+
+	requestBody := data{}
 	newCR := models.CR{}
-	c.Bind(&newCR)
+
+	c.Bind(&requestBody)
+
+	newCR.LocationID = requestBody.locationid
+	newCR.Floor = requestBody.floor
+	newCR.Gender = requestBody.gender
 
 	database.Db.Debug().Create(&newCR)
+	database.Db.First(&newCR)
+
+	newFacilityAvailable := models.FacilityAvailable{CRid: newCR.ID}
+	for _, fid := range requestBody.facilities {
+		newFacilityAvailable.Fid = fid
+		database.Db.Create(&newFacilityAvailable)
+	}
 
 	return returnData(c, newCR)
 }
